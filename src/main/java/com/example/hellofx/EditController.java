134b900909620ba785fd7176a46c0c884ce85158
mpEmperor <multaniprabnoor71@gmail.com>
2022-05-31 01:01:06 -0400

    started working on edit profile capabilities.

--- src/main/java/com/example/hellofx/EditController.java
+++ src/main/java/com/example/hellofx/EditController.java
@@ -3,6 +3,9 @@ package com.example.hellofx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 
 public class EditController {
     @FXML
@@ -15,7 +18,10 @@ public class EditController {
     private Label theme;
     @FXML
     private Label birthday;
+    @FXML
+    private AnchorPane scenePane;
     public void initialize() {
+        scenePane.setBackground(new Background(new BackgroundFill(LogoutController.getAccount().getTheme(), null, null)));
         username.setText((String) LogoutController.getAccount().getUserPass().keySet().toArray()[0]);
         password.setText(LogoutController.getAccount().getUserPass().get(username.getText()));
         avatarView.setImage(LogoutController.getAccount().getAvatar());
